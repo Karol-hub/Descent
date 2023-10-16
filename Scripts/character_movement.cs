a03910f5d659fb6def11a82e1236a71719e0dd41
Karol-hub <127150441+Karol-hub@users.noreply.github.com>
2023-10-16 16:53:48 +0100

    small steps

--- Scripts/character_movement.cs
+++ Scripts/character_movement.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class CharacterCont : CharacterBody2D
+public partial class character_movement : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
@@ -17,23 +17,25 @@ public partial class CharacterCont : CharacterBody2D
 		if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0f, Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+
+	private void PlayerInput()
+	{
+        // Handle Jump.
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+            velocity.Y = JumpVelocity;
+    }
 }
