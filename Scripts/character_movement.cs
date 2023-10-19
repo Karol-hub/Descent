2487e355db78c03daf6a1206505afade7bd04b4d
Karol-hub <127150441+Karol-hub@users.noreply.github.com>
2023-10-19 15:36:48 +0100

    men

--- Scripts/character_movement.cs
+++ Scripts/character_movement.cs
@@ -3,22 +3,20 @@ using System;
 
 public partial class character_movement : CharacterBody2D
 {
+	//Constants
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
-
-	public override void _PhysicsProcess(double delta)
+    public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	//Presets
+    private Vector2 velocity;
+    private Vector2 direction;
+	private Vector2 facing;
+    public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
+        PlayerInput();
+        // Add the gravity.
+        if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
-
-		// Get the input direction and handle the movement/deceleration.
-		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -34,6 +32,7 @@ public partial class character_movement : CharacterBody2D
 
 	private void PlayerInput()
 	{
+        direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
         // Handle Jump.
         if (Input.IsActionJustPressed("jump") && IsOnFloor())
             velocity.Y = JumpVelocity;
