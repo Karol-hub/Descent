78ac8bab27b09f2befca08d33bcfc2e66e841717
Karol-hub <127150441+Karol-hub@users.noreply.github.com>
2023-10-23 14:04:27 +0100

    bit o progress

--- Scripts/character_movement.cs
+++ Scripts/character_movement.cs
@@ -9,20 +9,25 @@ public partial class character_movement : CharacterBody2D
     private Vector2 velocity;
     private Vector2 direction;
 	private bool facingLeft;
-    private Sprite2D sprite = ;
+    private Sprite2D sprite = GetNode("CharacterSprite");
     //Character presets
     public float Speed = 300.0f;
     public float JumpVelocity = -400.0f;
+    public float dashVelocity = 400f;
     public override void _Process(double delta)
     {
         PlayerInput();
     }
     public override void _PhysicsProcess(double delta)
 	{
+        #region gravity
         // Add the gravity.
         if (!IsOnFloor())
 			velocity.Y += gravity * (float)delta;
-		if (direction != Vector2.Zero)
+        #endregion
+
+        #region move_player
+        if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
 		}
@@ -30,13 +35,15 @@ public partial class character_movement : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0f, Speed);
 		}
+        #endregion
 
-		Velocity = velocity;
+        Velocity = velocity;
 		MoveAndSlide();
 	}
 
 	private void PlayerInput()
 	{
+        #region Moving
         //gets direction of player input
         direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
         if (direction.X == -1f)
@@ -47,11 +54,14 @@ public partial class character_movement : CharacterBody2D
         {
             facingLeft = false;
         }
+        #endregion
 
+        #region jumping
         // Handle Jump.
         if (Input.IsActionJustPressed("jump") && IsOnFloor())
         {
             velocity.Y = JumpVelocity;
         }
+        #endregion
     }
 }
