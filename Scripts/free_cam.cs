9c389c8efd6c4956aa821df319675052eae33405
Karol-hub <127150441+Karol-hub@users.noreply.github.com>
2023-12-04 12:57:11 +0000

    generation starting maybe

---
+++ Scripts/free_cam.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class free_cam : Node2D
+{
+    private Node2D self;
+    private float speedy;
+    private Vector2 direction;
+    private float multiplier = 5f;
+
+    public override void _Ready()
+    {
+        self = GetNode<Node2D>(".");
+    }
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionPressed("dash"))
+        {
+            speedy = multiplier * (float)delta;
+        }
+        else
+        {
+            speedy = (float)delta;
+        }
+        direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        self.Position += direction * speedy *300f;
+    }
+}
