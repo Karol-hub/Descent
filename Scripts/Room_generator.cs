4758c21ff0d8cce668328c8c5402d8a5236d26ab
Karol-hub <jeziorczak.karol@gmail.com>
2023-12-05 20:10:29 +0000

    progress on generation
    - zoom on freecam script
    - Script for spreading works now
    - Only spreads horizontally though
    - Made it so that a certain percentage of rooms is removed after rooms are spread

--- Scripts/Room_generator.cs
+++ Scripts/Room_generator.cs
@@ -10,35 +10,43 @@ public partial class Room_generator : Node2D
     private float minXScale = 8f;
     private float maxYScale = 20f;
     private float minYScale = 8f;
+	private float spreadFactor = 100f;
 	//spreading rooms apart
 	private bool spread = false;
 	private int count = 0;
 	private Vector2 direction = Vector2.Zero;
+	private Vector2 displacement;
 	private float step = 1f;
-	//temp statistics stuff
+	//temp statistics stuff Remove at end
 	private int loopCount = 0;
+	//Deleting rooms
+	private float deletingRoomsFactor = 0.5f; //Needs to be between 0-1
+	private bool removedRooms;
     public override void _Ready()
 	{
 		var rng = new RandomNumberGenerator();
 		rng.Randomize();
         var room = GD.Load<PackedScene>("res://Scenes/room_generator.tscn");
+		spreadFactor = amountOfRooms / 5f;
 
 		for (int i = 0; i < amountOfRooms; i++) //makes a certain amount of "rooms"
 		{
 			var instance = room.Instantiate();
 			AddChild(instance);
 			instance.GetNode<CollisionShape2D>("Collision").Scale = new Vector2(Map(rng.Randf(),minXScale,maxXScale), Map(rng.Randf(), minYScale, maxYScale)); //generates random scale
-            instance.GetNode<Area2D>(".").Position = new Vector2(rng.Randf()*10, rng.Randf())*10; //generates initial random position
+            instance.GetNode<Area2D>(".").Position = new Vector2(rng.Randf()*spreadFactor, rng.Randf())*spreadFactor; //generates initial random position
         }
         GenerateRooms();
 	}
 	public async void GenerateRooms()
 	{
+        var rng = new RandomNumberGenerator();
+        rng.Randomize();
         //this only runs once at the start of the game so the inefficiency is loading time
         while (!spread) //if the rooms have overlapping areas
 		{
-            loopCount += 1;
-			step = 2 + (4000/(((0.1f*MathF.Pow(loopCount,2f))+200)));
+            //loopCount += 1;
+			//step = 2 + (4000/(((0.1f*MathF.Pow(loopCount,2f))+200)));
             await Task.Delay(TimeSpan.FromMilliseconds(1));
             count = 0; //resets count to check overlapping areas of child
             for (int i = 0; i < amountOfRooms; i++)
@@ -48,9 +56,11 @@ public partial class Room_generator : Node2D
 					direction = Vector2.Zero; //resets direction to 0
 					for (int j = 0; j < GetChild<Area2D>(i).GetOverlappingAreas().Count; j++) //check every overlapping area
 					{
-                        direction += GetChild<Area2D>(i).Position - GetChild<Area2D>(i).GetOverlappingAreas()[j].Position; //finds difference between original area and overlapping area
+						displacement = GetChild<Area2D>(i).Position - GetChild<Area2D>(i).GetOverlappingAreas()[j].Position;
+                        direction += (10/displacement.Length()) * displacement.Normalized(); //finds difference between original area and overlapping area
                     }
                     direction = direction.Normalized();
+					//loopCount += 1;
 					GetChild<Area2D>(i).Position += direction * step;
                 }
 				else
@@ -61,11 +71,16 @@ public partial class Room_generator : Node2D
 			if (count == amountOfRooms)
 			{
 				spread = true;
-                GD.Print("fin");
-				GD.Print(loopCount);
+                //GD.Print("fin");
+				//GD.Print(loopCount);
             }
         }
-	}
+        
+		for (int i = 0; i < (amountOfRooms * deletingRoomsFactor); i++)
+		{
+			RemoveChild(GetChild<Area2D>(rng.RandiRange(0, (int)(amountOfRooms * deletingRoomsFactor) - i)));
+		}
+    }
 	private float Map(float from, float min, float max)
 	{
 		return min+ (from * (max-min));
--- Scripts/free_cam.cs
+++ Scripts/free_cam.cs
@@ -8,10 +8,12 @@ public partial class free_cam : Node2D
     private Vector2 direction;
     private float multiplier = 5f;
     private Camera2D cam;
+    private float zoomStep = 0.1f;
 
     public override void _Ready()
     {
         self = GetNode<Node2D>(".");
+        cam = GetNode<Camera2D>("./Camera2D");
     }
     public override void _Process(double delta)
     {
@@ -23,7 +25,15 @@ public partial class free_cam : Node2D
         {
             speedy = (float)delta;
         }
+        if (Input.IsActionJustPressed("zoom_out"))
+        {
+            cam.Zoom -= new Vector2(zoomStep,zoomStep);
+        }
+        if (Input.IsActionJustPressed("zoom_in"))
+        {
+            cam.Zoom += new Vector2(zoomStep, zoomStep);
+        }
         direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-        self.Position += direction * speedy *300f;
+        self.Position += direction * speedy * (1/cam.Zoom.X) *300f;
     }
 }
